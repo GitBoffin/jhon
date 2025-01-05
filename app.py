554f467110acdbca5da40461a30a9b94c58a8387
GitBoffin <158105068+GitBoffin@users.noreply.github.com>
2025-01-05 05:27:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -27,4 +28,5 @@ def wallet():
 
 # Start the Flask app
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from Heroku or default to 5000
+    app.run(host="0.0.0.0", port=port, debug=False)  # Disable debug mode in production
